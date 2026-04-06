afdf5075257ac4212ef1263bcd283e0cdb9d462d
solarlime <solarlime@outlook.com>
2026-04-06 20:29:01 +0300

    feat: added an example for rsbuild

---
+++ examples/rsbuild/rsbuild.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+import { pluginSolid } from '@rsbuild/plugin-solid';
+import { rsbuildPluginSolidSvg } from 'rspack-plugin-solid-svg';
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  source: {
+    entry: {
+      index: './src/App.tsx',
+    },
+  },
+  server: {
+    open: false,
+    port: 3003,
+  },
+  plugins: [
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+      babelLoaderOptions: {
+        presets: ['solid'],
+      },
+    }),
+    pluginSolid(),
+    rsbuildPluginSolidSvg({
+      svgo: { enabled: false },
+    }),
+  ],
+});
