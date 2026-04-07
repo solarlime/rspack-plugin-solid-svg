2ddf4046e6472e302d738ecb10fb94ab5b49f003
solarlime <solarlime@outlook.com>
2026-04-07 23:21:10 +0300

    feat: added commonjs export

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,8 @@
+import { writeFileSync } from 'node:fs';
 import { defineConfig, type UserConfig } from 'tsdown';
 
 const restOptions: Omit<UserConfig, 'entry'> = {
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   clean: true,
   minify: false,
@@ -11,7 +12,13 @@ const restOptions: Omit<UserConfig, 'entry'> = {
 };
 
 export default defineConfig([
-  { entry: 'src/index.ts', ...restOptions },
+  {
+    entry: 'src/index.ts',
+    onSuccess: () => {
+      writeFileSync('dist/index.d.ts', "export * from './index.d.mts';\n");
+    },
+    ...restOptions,
+  },
   { entry: 'src/rspack.ts', ...restOptions },
   { entry: 'src/rsbuild.ts', ...restOptions },
   { entry: 'src/loader.ts', ...restOptions },
