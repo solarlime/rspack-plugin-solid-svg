ecc0def74cc3647d9bd55eb75b70b31c0db64857
solarlime <solarlime@outlook.com>
2026-05-23 19:07:06 +0300

    feat: prepared for rspack 2 & rsbuild 2

---
+++ examples/rsbuild-v2/rsbuild.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+import { pluginSolid } from '@rsbuild/plugin-solid';
+import { rsbuildPluginSolidSvg } from 'rspack-plugin-solid-svg/rsbuild';
+
+// import { rsbuildPluginSolidSvg } from 'rspack-plugin-solid-svg'; // another way
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  source: {
+    entry: {
+      index: './src/App.tsx',
+    },
+  },
+  html: {
+    title: 'Solid SVG Plugin Test',
+  },
+  server: {
+    open: false,
+    port: 3005,
+  },
+  plugins: [
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+      babelLoaderOptions: {
+        presets: ['solid'],
+      },
+    }),
+    pluginSolid(),
+    rsbuildPluginSolidSvg({
+      svgo: { enabled: false },
+    }),
+  ],
+});
---
+++ examples/rspack-v2/rspack.config.ts
@@ -0,0 +1,64 @@
+import { defineConfig } from '@rspack/cli';
+import { type RspackOptions, rspack } from '@rspack/core';
+import { RspackPluginSolidSvg } from 'rspack-plugin-solid-svg';
+
+// import { RspackPluginSolidSvg } from 'rspack-plugin-solid-svg/rspack'; // another way
+
+const config: RspackOptions = defineConfig({
+  entry: {
+    main: './src/App.tsx',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    publicPath: '/',
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['solid'],
+              plugins: ['solid-refresh/babel'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new RspackPluginSolidSvg({
+      svgo: {
+        enabled: true,
+        svgoConfig: {
+          plugins: [
+            'removeXMLProcInst',
+            'removeMetadata',
+            'removeDoctype',
+            {
+              name: 'cleanupIds',
+              params: {
+                preserve: ['helpful'],
+              },
+            },
+          ],
+        },
+      },
+    }),
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    port: 3004,
+    hot: true,
+    open: false,
+  },
+});
+
+export default config;
